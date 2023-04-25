7dd43e94225a6a4ccf3aae04c07b6557c06d137e
rifatNR <Rifatnoor92@gmail.com>
2023-04-25 06:49:11 +0600

    Init - Typescript, Nativewind Done

---
+++ App.tsx
@@ -0,0 +1,49 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { Button, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-red-500">
+
+      <Text className="bg-blue-500">Open up App.js to start working on your app!</Text>
+      <Text className="bg-pink-500">Open up App.js to start working on your app!</Text>
+      <Text className="bg-indigo-500">Open up App.js to start working on your app!</Text>
+      <Text className="bg-yellow-500">Open up App.js to start working on your app!</Text>
+      <Text className="bg-emerald-500">Open up App.js to start working on your app!</Text>
+
+      <Text className="bg-blue-500">Open up App.js to start working on your app!</Text>
+      <Text className="bg-pink-500">Open up App.js to start working on your app!</Text>
+      <Text className="bg-indigo-500">Open up App.js to start working on your app!</Text>
+      <Text className="bg-yellow-500">Open up App.js to start working on your app!</Text>
+      <Text className="bg-emerald-500">Open up App.js to start working on your app!</Text>
+
+      <Text className="bg-blue-500">Open up App.js to start working on your app!</Text>
+      <Text className="bg-pink-500">Open up App.js to start working on your app!</Text>
+      <Text className="bg-indigo-500">Open up App.js to start working on your app!</Text>
+      <Text className="bg-yellow-500">Open up App.js to start working on your app!</Text>
+      <Text className="bg-emerald-500">Open up App.js to start working on your app!</Text>
+
+      <Text className="bg-blue-500">Open up App.js to start working on your app!</Text>
+      <Text className="bg-pink-500">Open up App.js to start working on your app!</Text>
+      <Text className="bg-indigo-500">Open up App.js to start working on your app!</Text>
+      <Text className="bg-yellow-500">Open up App.js to start working on your app!</Text>
+      <Text className="bg-emerald-500">Open up App.js to start working on your app!</Text>
+
+      <Text className="bg-blue-500">Open up App.js to start working on your app!</Text>
+      <Text className="bg-pink-500">Open up App.js to start working on your app!</Text>
+      <Text className="bg-indigo-500">Open up App.js to start working on your app!</Text>
+      <Text className="bg-yellow-500">Open up App.js to start working on your app!</Text>
+      <Text className="bg-emerald-500">Open up App.js to start working on your app!</Text>
+
+      <Text className="bg-blue-500">Open up App.js to start working on your app!</Text>
+      <Text className="bg-pink-500">Open up App.js to start working on your app!</Text>
+      <Text className="bg-indigo-500">Open up App.js to start working on your app!</Text>
+      <Text className="bg-yellow-500">Open up App.js to start working on your app!</Text>
+      <Text className="bg-emerald-500">Open up App.js to start working on your app!</Text>
+
+      <Button title='Hello'></Button>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    content: ["./.{js,jsx,ts,tsx}", "./App.{js,jsx,ts,tsx}", "./<custom directory>/**/*.{js,jsx,ts,tsx}"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
